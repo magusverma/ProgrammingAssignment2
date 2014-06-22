731cd1f9f1bfdf3ab92c3632548edf257a72d470
Magus Verma <magusverma.2@gmail.com>
2014-06-23 01:59:45 +0530

    Added Comments and Cleaned Variable names

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Code Written by Magus Verma for R Course of Coursera , Week 2 Programming Assignment 
 
-## Write a short comment describing this function
-
+## This defines the special "matrix" object needed for assignment 
+# Start with something like x <- makeCacheMatrix()
 makeCacheMatrix <- function(x = matrix()) {
-	m <- NULL
+	# m holds the inverse of the matrix x
+	inverse_of_x <- NULL
+
+	# Setting matrix for a makeCacheMatrix object
+	# run using something like
+	# x$set(matrix(1:4,2,2))
     set <- function(y) {
             x <<- y
-            m <<- NULL
+            inverse_of_x <<- NULL
     }
+    
+    # Gettings matrix for a makeCacheMatrix object
+	# run using something like
+	# x$get()
     get <- function() x
-    setsolve <- function(solve) m <<- solve
-    getsolve <- function() m
+
+    # setsolve,getsolve are used setter and getter for inverse of x 
+    setsolve <- function(solve) inverse_of_x <<- solve
+    getsolve <- function() inverse_of_x
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+# To verify run something like following twice , if you have an object like x of makeCacheMatrix already up
+# cacheSolve(x) 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    m <- x$getsolve()
-    if(!is.null(m)) {
+    
+    inverse_of_x <- x$getsolve()
+    if(!is.null(inverse_of_x)) {
             message("getting cached data")
-            return(m)
+            return(inverse_of_x)
     }
     data <- x$get()
-    m <- solve(data, ...)
-    x$setsolve(m)
-    m
+    inverse_of_x <- solve(data, ...)
+    x$setsolve(inverse_of_x)
+    inverse_of_x
 }
+
+# inverse can be checked using something like
+# cacheSolve(x) %*% x$get()
